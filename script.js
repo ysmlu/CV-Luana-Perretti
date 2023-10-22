2d3ca3cfcbf88f6adda2ec2efce231186c15fe96
LuanaPerretti <luanaperretti31@gmail.com>
2023-10-22 19:16:14 -0300

    Intentando cambiar el cursor

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 // JavaScript Document
+
+const cursor = document.querySelector(".custom-cursor"); /*No entiendo porque el cursor no funciona, probé un montón de formas*/
+			cursor.style.width = "30px";
+			cursor.style.height = "30px";
+			cursor.style.backgroundImage = "url('img/estrella.puntero-17.svg')";
+
+			const x = document.querySelector("body").clientX;
+			const y = document.querySelector("body").clientY;
+
+			cursor.style.left = x + 10;
+			cursor.style.top = y + 10;
+
+			document.body.appendChild(cursor);
