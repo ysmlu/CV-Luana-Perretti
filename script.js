65955415759a294ba78378488dcce017a81edbc8
LuanaPerretti <luanaperretti31@gmail.com>
2023-10-24 18:53:36 -0300

    Probando animación en título

--- script.js
+++ script.js
@@ -24,4 +24,18 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 
+const titulo = document.querySelector(".h2sobremi");
 
+// Desplaza el título desde la izquierda a la pantalla
+titulo.style.left = "-100%";
+
+// Realiza la transición en 2 segundos
+titulo.style.transition = "left 2s";
+
+// Oculta el título al principio
+titulo.style.display = "none";
+
+// Muestra el título después de 2 segundos
+setTimeout(() => {
+  titulo.style.display = "block";
+}, 2000);
